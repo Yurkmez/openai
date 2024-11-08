776774f1346dd1f3f601e370d6a21a9aa765059a
Bogdan Stashchuk <bstashchuk@gmail.com>
2024-11-08 19:27:48 +0000

    Added error handling in example.mjs

--- node/example.mjs
+++ node/example.mjs
@@ -1,29 +1,45 @@
-import OpenAI from "openai";
+import OpenAI, { OpenAIError } from "openai";
 import "dotenv/config";
+
+if (!process.env.OPENAI_API_KEY) {
+  console.error(
+    "Error: The OPENAI_API_KEY environment variable is missing or empty.\nPlease add it to the .env file."
+  );
+  process.exit(1);
+}
 
 const openai = new OpenAI();
 
 // console.log(process.env.OPENAI_API_KEY);
+const systemContent = "Старайся отвечать кратко. Не более 15 слов";
+const userContent = "Напиши историю о себе";
 
-const completion = await openai.chat.completions.create({
-  model: "gpt-4o-mini",
-  max_tokens: 300,
-  n: 2,
-  messages: [
-    {
-      role: "system",
-      content:
-        "Ты настроен на веселый разговор. Выводи только одну короткую историю не больше 20 слов",
-    },
-    {
-      role: "user",
-      content: "Напиши историю о программистах",
-    },
-  ],
-});
+try {
+  const completion = await openai.chat.completions.create({
+    model: "gpt-4o-mini",
+    max_tokens: 300,
+    n: 2,
+    messages: [
+      {
+        role: "system",
+        content: systemContent,
+      },
+      {
+        role: "user",
+        content: userContent,
+      },
+    ],
+  });
 
-console.log(completion);
+  // console.log(completion);
 
-completion.choices.forEach((choice) => {
-  console.log(choice.message);
-});
+  completion.choices.forEach((choice) => {
+    console.log(choice.message);
+  });
+} catch (error) {
+  if (error instanceof OpenAIError) {
+    console.error("OpenAI Error:", error.message);
+  } else {
+    console.error("Some error occured:", error.message);
+  }
+}
