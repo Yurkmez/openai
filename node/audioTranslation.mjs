23ec0347b2deb7e13434129654c33960a04b5a7b
Bogdan Stashchuk <bstashchuk@gmail.com>
2024-11-11 19:56:47 +0000

    Using translation API to translate audio using Whisper model

---
+++ node/audioTranslation.mjs
@@ -0,0 +1,26 @@
+import fs from "fs";
+import path from "path";
+import openai from "./utils/openai.mjs";
+import { openaiErrorHandler } from "./utils/openaiErrorHandler.mjs";
+
+const __dirname = import.meta.dirname;
+
+const model = "whisper-1";
+const fileName = "test_v1.m4a";
+
+const dir = path.join(__dirname, "audio", "m4a");
+const filePath = path.join(dir, fileName);
+
+try {
+  const response = await openai.audio.translations.create({
+    model,
+    file: fs.createReadStream(filePath),
+    response_format: "json", // default
+  });
+
+  console.log(response);
+
+  fs.appendFileSync(path.join(dir, "translations.txt"), `${response.text}\n\n`);
+} catch (error) {
+  openaiErrorHandler(error);
+}
